295fb4f1d9f5e3bf3e3184a57f9126e0575bf000
Marcela Salvador <salvadormarcela18@gmail.com>
2022-11-17 15:01:32 -0600

    lab

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -11,6 +11,25 @@ router.get("/characters", (req, res, next) => {
     .catch(err => console.error(err))
 });
 
+//Create new character
+router.post("/characters/create", (req,res,next) => {
+    axios.post("https://ih-crud-api.herokuapp.com/characters", {
+    name: "Harry Potter",
+    occupation: "master wizard",
+    weapon: "magic",
+    debt: false
+
+})
+    .then(responseFromApi => {
+        res.render("characters", { characters: responseFromAPI.data });
+    })
+        
+    .catch (err => console.log(err))
+
+})
+
+
+//character details page
 
 router.get("/characters/:id", (req, res, next) => {
     axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
@@ -20,6 +39,48 @@ router.get("/characters/:id", (req, res, next) => {
     })
     .catch(err => console.error(err))
 });
+
+//Update Character
+
+router.post("/characters/:id", (req, res, next) => {
+    axios.post(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    .then(responseFromAPI => {
+        // console.log("details: ", responseFromAPI.data)
+        res.render("characters/:id", { character: responseFromAPI.data });
+    })
+    .catch(err => console.error(err))
+});
+
+router.get("/characters/:id/edit", (req, res, next) => {
+    axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    .then(responseFromAPI => {
+        // console.log(responseFromAPI)
+        res.render("edit-character", { character: responseFromAPI.data });
+    })
+    .catch(err => console.error(err))
+});
+
+
+router.post("/characters/:id/update", (req, res, next) => {
+
+    axios.put(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`,{
+        "name": "Henry"
+
+    })
+
+    .then(responseFromAPI => {
+        
+        res.render("/characters/:id", { character: responseFromAPI.data });
+    })
+    .catch(err => console.error(err))
+
+})
+
+
+// Delete character
+
+
+
 
 module.exports = router;
 
